ee32277c21f2c01dfccc25da155f5667ffdfd8bf
wouldyoueverfly <cpptypxe@ya.ru>
2025-06-21 21:37:19 +0300

    Initial Commit

---
+++ TouchesAndGesturesUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouchesAndGesturesUIKit
+//
+//  Created by Ruslan Khusainov on 21.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
